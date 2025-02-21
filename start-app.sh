10955b211c5e67ae90de1cb787f784666b880f03
enrique.amador <enrique.amador@spectrumeffect.com>
2025-02-20 19:27:32 -0600

    dumping logs

--- start-app.sh
+++ start-app.sh
@@ -1,5 +1,8 @@
 # Build the Docker image:
-docker build -t java-gc-example .
+# docker build -t java-gc-example .
 
 # Run the container:
-docker run --name java-gc-example-1 java-gc-example
+# docker run --name java-gc-example-1 java-gc-example
+
+# Build and run the container:
+docker-compose up --build
